28921a8f67dc544af363696eaf034bcd60e4a309
Ogheneochuko <7204868+amochuko@users.noreply.github.com>
2024-07-23 21:23:56 +0100

    fix: unit test updates

--- jest.config.js
+++ jest.config.js
@@ -4,10 +4,10 @@ const createJestConfig = nextJest({ dir: './' });
 
 /** @type {import('jest').Config}*/
 const customJestConfig = {
+  setupFilesAfterEnv: ['<rootDir>/jest-setup.js'],
   moduleDirectories: ['node_modules', '<rootDir>/'],
   testPathIgnorePatterns:  ['<rootDir>/e2e'], // ignores e2e
   testEnvironment: 'jest-environment-jsdom',
-  collectCoverage: true,
   collectCoverageFrom: [
     '**/*.{js,jsx}',
     '!**/node_modules/**',
